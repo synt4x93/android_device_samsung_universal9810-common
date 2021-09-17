a66018fb3d85ae722d0aec724f3cfe9a8d70c85f
synt4x93 <synt4x.93@gmail.com>
2021-09-17 14:38:41 +0100

    universal9810: tidy up again
    Change-Id: Ieda3d4f4b106247198474fe67836c98441f06c3f

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -14,27 +14,12 @@
 # limitations under the License.
 #
 
+# Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-ifneq ($(findstring lineage, $(TARGET_PRODUCT)),)
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
-endif
-
-# Permissions
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/permissions/platform-samsung.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/platform-samsung.xml \
-    frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.bluetooth.xml \
-    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.bluetooth_le.xml \
-    frameworks/native/data/etc/android.hardware.ethernet.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.ethernet.xml \
-    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.fingerprint.xml \
-    frameworks/native/data/etc/android.hardware.sensor.heartrate.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.sensor.heartrate.xml \
-    frameworks/native/data/etc/android.hardware.vr.high_performance.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.vr.high_performance.xml
-
 # Screen density
-# Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := xlarge
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
-# A list of dpis to select prebuilt apk, in precedence order.
 PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
 
 # Boot animation
@@ -43,9 +28,15 @@ TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 TARGET_SCREEN_HEIGHT := 2960
 TARGET_SCREEN_WIDTH := 1440
 
-# SP-NDK
+# Init
 PRODUCT_PACKAGES += \
-    libvulkan
+    fstab.zram \
+    init.samsung.rc \
+    init.usb_accessory.rc
+
+# Recovery
+PRODUCT_PACKAGES += \
+    init.recovery.samsungexynos9810.rc
 
 # Audio
 PRODUCT_COPY_FILES += \
@@ -54,32 +45,6 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     audio.a2dp.default
-
-# Camera
-PRODUCT_PACKAGES += \
-    Snap
-
-# Init
-PRODUCT_PACKAGES += \
-    fstab.zram \
-    init.samsung.rc \
-    init.usb_accessory.rc
-
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    lineage.livedisplay@2.0-service.samsung-exynos
-
-# LPM
-PRODUCT_PACKAGES += \
-    libsuspend
-
-# HotwordEnrollement app permissions
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-hotword.xml
-
-# Net
-PRODUCT_PACKAGES += \
-    netutils-wrapper-1.0
 
 # NFC
 PRODUCT_PACKAGES += \
@@ -91,29 +56,45 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf
 
-# Recovery
+# Camera
 PRODUCT_PACKAGES += \
-    init.recovery.samsungexynos9810.rc
+    Snap
 
-# SamsungDoze
+# Light
 PRODUCT_PACKAGES += \
-    SamsungDoze
+    android.hardware.light@2.0-service.9810
+
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    lineage.livedisplay@2.0-service.samsung-exynos
 
 # Touch
 PRODUCT_PACKAGES += \
     lineage.touch@1.0-service.9810
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    lineage.trust@1.0-service
-
 # FastCharge
 PRODUCT_PACKAGES += \
     lineage.fastcharge@1.0-service.9810
 
-# Lights
+# Trust
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.9810
+    lineage.trust@1.0-service
+
+# Net
+PRODUCT_PACKAGES += \
+    netutils-wrapper-1.0
+
+# SP-NDK
+PRODUCT_PACKAGES += \
+    libvulkan
+
+# LPM
+PRODUCT_PACKAGES += \
+    libsuspend
+
+# SamsungDoze
+PRODUCT_PACKAGES += \
+    SamsungDoze
 
 # S-Pen
 PRODUCT_COPY_FILES += \
@@ -122,6 +103,17 @@ PRODUCT_COPY_FILES += \
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio_keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio_keys.kl
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/permissions/platform-samsung.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/platform-samsung.xml \
+    $(LOCAL_PATH)/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-hotword.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.bluetooth.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.bluetooth_le.xml \
+    frameworks/native/data/etc/android.hardware.ethernet.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.ethernet.xml \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.fingerprint.xml \
+    frameworks/native/data/etc/android.hardware.sensor.heartrate.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.sensor.heartrate.xml \
+    frameworks/native/data/etc/android.hardware.vr.high_performance.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.vr.high_performance.xml
 
 # Soong
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
