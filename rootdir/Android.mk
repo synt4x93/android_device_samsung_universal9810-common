f14635f517d6078b9f5cf171c7af7c6eaaf0b0f7
synt4x93 <synt4x.93@gmail.com>
2021-09-17 08:54:36 +0100

    universal9810: init: import init.samsungexynos9810.usb.rc
    - N960FXXS8FUF5 / G960FXXUGFUG4 / G965FXXUGFUG4
    
    Change-Id: I11ce48aa8d1b9d59182a6b1bb7c5adfb417a3b73

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -33,6 +33,14 @@ LOCAL_MODULE_PATH       := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE            := init.samsungexynos9810.usb.rc
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/init.samsungexynos9810.usb.rc
+LOCAL_MODULE_PATH       := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/init
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE            := init.samsung.rc
 LOCAL_MODULE_TAGS       := optional
 LOCAL_MODULE_CLASS      := ETC
